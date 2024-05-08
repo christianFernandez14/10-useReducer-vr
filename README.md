2d632ecbd80d620afdc1f184ed61c947b7c671d1
Christian Fernandez <christian14@gmail.com>
2024-05-07 21:07:27 -0400

    First commit / About useReducer / limpiando entorno de trabajo

---
+++ README.md
@@ -0,0 +1,3 @@
+Trabajando con el useReducer
+
+1.- Es el hook que te permite trabajar de otra manera los estados.
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+
+const App = () => {
+
+  return (
+    <div>
+      <h1>From App Component</h1>
+    </div>
+  )
+}
+
+export default App
